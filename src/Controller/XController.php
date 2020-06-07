6b47cdc3b7c0ea14ba298cfa12745a4787039629
Malte Zacharias <me@malte-zacharias.de>
2020-06-07 06:39:10 +0200

    First version

---
+++ src/Controller/XController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class XController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function list()
+    {
+        $number = random_int(0, 100);
+        return $this->render('login.html.twig',[]);
+    }
+}
