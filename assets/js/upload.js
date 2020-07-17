a244d913274d7356f9fe766dc22fef06e6bfcc91
luukmed <53969355+luukmed@users.noreply.github.com>
2020-07-17 22:18:02 +0200

    Update array handling in uploads
    Change from for-loops to forEach

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -4,26 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
   // dynamically populate course names
   let courseNameSelectElement = document.getElementById("course_name");
   courses.forEach(course => {
-    let courseoption = document.createElement("option");
-    courseoption.value = course.course_number;
-    courseoption.text = course.course_name;
-    courseNameSelectElement.appendChild(courseoption);
+    let courseOption = document.createElement("option");
+    courseOption.value = course.course_number;
+    courseOption.text = course.course_name;
+    courseNameSelectElement.appendChild(courseOption);
   })
 
   // dynamically populate lecture names for chosen course
-  let lecture_name_select = document.querySelector('#lecture_name');
-  course_name_select.addEventListener("change", function() {
-    let selected_course = this.value;
-    while (lecture_name_select.firstChild) {
-      lecture_name_select.removeChild(lecture_name_select.firstChild);
+  let lectureNameSelectElement = document.getElementById("lecture_name");
+  courseNameSelectElement.addEventListener("change", function() {
+    let selectedCourse = this.value;
+    while (lectureNameSelectElement.firstChild) {
+      lectureNameSelectElement.removeChild(lectureNameSelectElement.firstChild);
     }
-    for (let j=0; j < courses[this.value].lectures.length; j++) {
-      let lectureoption = document.createElement("option");
-      lectureoption.value = courses[this.value].lectures[j].lecture_id;
-      lectureoption.text = courses[this.value].lectures[j].lecture_name;
-      lecture_name_select.appendChild(lectureoption);
-    }
-
+    courses[selectedCourse].lectures.forEach(lecture => {
+      let lectureOption = document.createElement("option");
+      lectureOption.value = lecture.lecture_id;
+      lectureOption.text = lecture.lecture_name; 
+      lectureNameSelectElement.appendChild(lectureOption);
+    });
   });
 });
-
